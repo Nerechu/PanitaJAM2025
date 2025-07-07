3eaaa5b1c3f3d56daf4f732e11129a0b778281fb
Xad <xadnarr@gmail.com>
2025-07-07 18:28:44 +0200

    No explotes porfa
    - Falta la animacion procedural
    - Poner las cosas bonicas D:

---
+++ Assets/Procedural_Ivy/Assets/Scripts/PlantGun.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlantGun : MonoBehaviour
+{
+    [Header("Disparo")]
+    public Camera fpsCamera;
+    public Transform shotOrigin;
+    public ProceduralIvy ivySystem;
+    public LayerMask plantLayer;
+    public float maxDistance = 100f;
+
+    [Header("Feedback Visual")]
+    public LineRenderer shotLine;
+    public float shotDuration = 0.05f;
+
+    [Header("Impacto")]
+    public GameObject impactParticlePrefab; // ← NUEVO: prefab de partícula de impacto
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = new Ray(fpsCamera.transform.position, fpsCamera.transform.forward);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, maxDistance))
+            {
+                // Línea del disparo
+                StartCoroutine(ShowShotRay(shotOrigin.position, hit.point));
+
+                // Efecto de impacto visual
+                SpawnImpactParticle(hit.point, hit.normal);
+
+                // Interacción con capa de plantas
+                if (((1 << hit.collider.gameObject.layer) & plantLayer) != 0)
+                {
+                    ivySystem.createIvy(hit);
+                    ivySystem.combineAndClear();
+                }
+            }
+            else
+            {
+                // Disparo al aire
+                StartCoroutine(ShowShotRay(shotOrigin.position, shotOrigin.position + fpsCamera.transform.forward * maxDistance));
+            }
+        }
+    }
+
+    void SpawnImpactParticle(Vector3 position, Vector3 normal)
+    {
+        if (impactParticlePrefab != null)
+        {
+            GameObject impact = Instantiate(impactParticlePrefab, position, Quaternion.LookRotation(normal));
+            Destroy(impact, 2f); // Destruir después de 2 segundos
+        }
+    }
+
+    IEnumerator ShowShotRay(Vector3 start, Vector3 end)
+    {
+        if (shotLine == null)
+            yield break;
+
+        shotLine.SetPosition(0, start);
+        shotLine.SetPosition(1, end);
+        shotLine.enabled = true;
+
+        yield return new WaitForSeconds(shotDuration);
+
+        shotLine.enabled = false;
+    }
+}
--- Assets/Procedural_Ivy/Assets/Scripts/ProceduralIvy.cs
+++ Assets/Procedural_Ivy/Assets/Scripts/ProceduralIvy.cs
@@ -2,111 +2,89 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ProceduralIvy : MonoBehaviour {
-
-    public Camera cam;
-    [Space]
+public class ProceduralIvy : MonoBehaviour
+{
     public float recycleInterval = 30;
-    [Space]
+
+    [Header("Branch Settings")]
     public int branches = 3;
     public int maxPointsForBranch = 20;
     public float segmentLength = .002f;
     public float branchRadius = 0.02f;
-    [Space]
+
+    [Header("Materials")]
     public Material branchMaterial;
     public Material leafMaterial;
     public Material flowerMaterial;
-    [Space]
+
+    [Header("Prefabs")]
     public Blossom leafPrefab;
     public Blossom flowerPrefab;
-    [Space]
+
+    [Header("Options")]
     public bool wantBlossoms;
-    [Space]
     public LayerMask plantLayer;
 
-    int ivyCount = 0;
+    private int ivyCount = 0;
 
-    void Update() {
-
-        if (Input.GetKeyUp(KeyCode.L)) {
-            // call this method when you are ready to group your meshes
-            combineAndClear();
-        }
-
-        if (Input.GetMouseButtonDown(0)) {
-            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if(Physics.Raycast(ray, out hit, 100, plantLayer)) {
-                createIvy(hit);
-            }
-        }
-    }
-
-    Vector3 findTangentFromArbitraryNormal(Vector3 normal) {
-        Vector3 t1 = Vector3.Cross(normal, Vector3.forward);
-        Vector3 t2 = Vector3.Cross(normal, Vector3.up);
-        if (t1.magnitude > t2.magnitude) {
-            return t1;
-        }
-        return t2;
-    }
-
-    public void createIvy(RaycastHit hit) {
+    // Este método lo llama el arma al disparar a un objeto en la capa "Plant"
+    public void createIvy(RaycastHit hit)
+    {
         Vector3 tangent = findTangentFromArbitraryNormal(hit.normal);
         GameObject ivy = new GameObject("Ivy " + ivyCount);
         ivy.transform.SetParent(transform);
-        for (int i = 0; i < branches; i++) {
+
+        for (int i = 0; i < branches; i++)
+        {
             Vector3 dir = Quaternion.AngleAxis(360 / branches * i + Random.Range(0, 360 / branches), hit.normal) * tangent;
             List<IvyNode> nodes = createBranch(maxPointsForBranch, hit.point, hit.normal, dir);
+
             GameObject branch = new GameObject("Branch " + i);
             Branch b = branch.AddComponent<Branch>();
-            if (!wantBlossoms) {
+
+            if (!wantBlossoms)
+            {
                 b.init(nodes, branchRadius, branchMaterial);
-            } else {
+            }
+            else
+            {
                 b.init(nodes, branchRadius, branchMaterial, leafMaterial, leafPrefab, flowerMaterial, flowerPrefab, i == 0);
             }
+
             branch.transform.SetParent(ivy.transform);
         }
 
         ivyCount++;
     }
 
-    Vector3 calculateTangent(Vector3 p0, Vector3 p1, Vector3 normal) {
-        var heading = p1 - p0;
-        var distance = heading.magnitude;
-        var direction = heading / distance;
-        return Vector3.Cross(normal, direction).normalized;
+    public void combineAndClear()
+    {
+        MeshManager.instance.combineUnder(transform);
+
+        foreach (Transform t in transform)
+        {
+            Destroy(t.gameObject);
+        }
     }
 
-    Vector3 applyCorrection(Vector3 p, Vector3 normal) {
-        return p + normal * 0.01f;
+    Vector3 findTangentFromArbitraryNormal(Vector3 normal)
+    {
+        Vector3 t1 = Vector3.Cross(normal, Vector3.forward);
+        Vector3 t2 = Vector3.Cross(normal, Vector3.up);
+        return (t1.magnitude > t2.magnitude) ? t1 : t2;
     }
 
-    bool isOccluded(Vector3 from, Vector3 to) {
-        Ray ray = new Ray(from, (to - from) / (to - from).magnitude);
-        return Physics.Raycast(ray, (to - from).magnitude);
-    }
-
-    bool isOccluded(Vector3 from, Vector3 to, Vector3 normal) {
-        return isOccluded(applyCorrection(from, normal), applyCorrection(to, normal));
-    }
-
-    Vector3 calculateMiddlePoint(Vector3 p0, Vector3 p1, Vector3 normal) {
-        Vector3 middle = (p0 + p1) / 2;
-        var h = p0 - p1;
-        var distance = h.magnitude;
-        var dir = h / distance;
-        return middle + normal * distance;
-    }
-
-    List<IvyNode> createBranch(int count, Vector3 pos, Vector3 normal, Vector3 dir) {
-
-        if (count == maxPointsForBranch) {
+    List<IvyNode> createBranch(int count, Vector3 pos, Vector3 normal, Vector3 dir)
+    {
+        if (count == maxPointsForBranch)
+        {
             IvyNode rootNode = new IvyNode(pos, normal);
             return new List<IvyNode> { rootNode }.join(createBranch(count - 1, pos, normal, dir));
-        } else if (count < maxPointsForBranch && count > 0) {
-
-            if (count % 2 == 0) {
+        }
+        else if (count < maxPointsForBranch && count > 0)
+        {
+            if (count % 2 == 0)
+            {
                 dir = Quaternion.AngleAxis(Random.Range(-20.0f, 20.0f), normal) * dir;
             }
 
@@ -114,25 +92,32 @@ public class ProceduralIvy : MonoBehaviour {
             Ray ray = new Ray(pos, normal);
             Vector3 p1 = pos + normal * segmentLength;
 
-            if (Physics.Raycast(ray, out hit, segmentLength)) {
+            if (Physics.Raycast(ray, out hit, segmentLength))
+            {
                 p1 = hit.point;
             }
+
             ray = new Ray(p1, dir);
 
-            if (Physics.Raycast(ray, out hit, segmentLength)) {
+            if (Physics.Raycast(ray, out hit, segmentLength))
+            {
                 Vector3 p2 = hit.point;
                 IvyNode p2Node = new IvyNode(p2, -dir);
                 return new List<IvyNode> { p2Node }.join(createBranch(count - 1, p2, -dir, normal));
-            } else {
+            }
+            else
+            {
                 Vector3 p2 = p1 + dir * segmentLength;
                 ray = new Ray(applyCorrection(p2, normal), -normal);
-                if (Physics.Raycast(ray, out hit, segmentLength)) {
+
+                if (Physics.Raycast(ray, out hit, segmentLength))
+                {
                     Vector3 p3 = hit.point;
                     IvyNode p3Node = new IvyNode(p3, normal);
 
-                    if (isOccluded(p3, pos, normal)) {
+                    if (isOccluded(p3, pos, normal))
+                    {
                         Vector3 middle = calculateMiddlePoint(p3, pos, (normal + dir) / 2);
-
                         Vector3 m0 = (pos + middle) / 2;
                         Vector3 m1 = (p3 + middle) / 2;
 
@@ -143,15 +128,19 @@ public class ProceduralIvy : MonoBehaviour {
                     }
 
                     return new List<IvyNode> { p3Node }.join(createBranch(count - 1, p3, normal, dir));
-                } else {
+                }
+                else
+                {
                     Vector3 p3 = p2 - normal * segmentLength;
                     ray = new Ray(applyCorrection(p3, normal), -normal);
 
-                    if (Physics.Raycast(ray, out hit, segmentLength)) {
+                    if (Physics.Raycast(ray, out hit, segmentLength))
+                    {
                         Vector3 p4 = hit.point;
                         IvyNode p4Node = new IvyNode(p4, normal);
 
-                        if (isOccluded(p4, pos, normal)) {
+                        if (isOccluded(p4, pos, normal))
+                        {
                             Vector3 middle = calculateMiddlePoint(p4, pos, (normal + dir) / 2);
                             Vector3 m0 = (pos + middle) / 2;
                             Vector3 m1 = (p4 + middle) / 2;
@@ -163,13 +152,15 @@ public class ProceduralIvy : MonoBehaviour {
                         }
 
                         return new List<IvyNode> { p4Node }.join(createBranch(count - 1, p4, normal, dir));
-                    } else {
+                    }
+                    else
+                    {
                         Vector3 p4 = p3 - normal * segmentLength;
                         IvyNode p4Node = new IvyNode(p4, dir);
 
-                        if (isOccluded(p4, pos, normal)) {
+                        if (isOccluded(p4, pos, normal))
+                        {
                             Vector3 middle = calculateMiddlePoint(p4, pos, (normal + dir) / 2);
-
                             Vector3 m0 = (pos + middle) / 2;
                             Vector3 m1 = (p4 + middle) / 2;
 
@@ -178,20 +169,36 @@ public class ProceduralIvy : MonoBehaviour {
 
                             return new List<IvyNode> { m0Node, m1Node, p4Node }.join(createBranch(count - 3, p4, dir, -normal));
                         }
+
                         return new List<IvyNode> { p4Node }.join(createBranch(count - 1, p4, dir, -normal));
                     }
                 }
             }
-
         }
+
         return null;
     }
 
-    void combineAndClear() {
-        MeshManager.instance.combineAll();
-        foreach (Transform t in transform) {
-            Destroy(t.gameObject);
-        }
+    bool isOccluded(Vector3 from, Vector3 to)
+    {
+        Ray ray = new Ray(from, (to - from).normalized);
+        return Physics.Raycast(ray, (to - from).magnitude);
     }
 
+    bool isOccluded(Vector3 from, Vector3 to, Vector3 normal)
+    {
+        return isOccluded(applyCorrection(from, normal), applyCorrection(to, normal));
+    }
+
+    Vector3 calculateMiddlePoint(Vector3 p0, Vector3 p1, Vector3 normal)
+    {
+        Vector3 middle = (p0 + p1) / 2;
+        float distance = (p0 - p1).magnitude;
+        return middle + normal * distance;
+    }
+
+    Vector3 applyCorrection(Vector3 p, Vector3 normal)
+    {
+        return p + normal * 0.01f;
+    }
 }
