1bebe424d2725b2cd978d6ceba79e57fc0c86801
Nerea <nerea.alon41@gmail.com>
2025-07-09 22:20:33 +0200

    Tool Push

---
+++ Assets/CityBuilderTool/Scripts/CityMeshCombiner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CityMeshCombiner : MonoBehaviour
+{
+    public void CombineCity()
+    {
+        if (GetComponent<CityScript>() != null)
+        {
+            CityScript CS = GetComponent<CityScript>();
+            if (CS.buildings.Count > 0)
+            {
+                for (int i = 0; i < CS.buildings.Count; i++)
+                {
+                    CS.buildings[i].GetComponent<MeshCombiner>().CombineAllMeshes();
+                }
+            }
+            GameObject StreetParent = GameObject.FindGameObjectWithTag("StreetParent");
+            StreetParent.AddComponent<MeshCombiner>();
+            MeshCombiner street_mesh_combiner = StreetParent.GetComponent<MeshCombiner>();
+            street_mesh_combiner.CombineAllMeshes();
+        }
+    }
+
+    public void RevertCity()
+    {
+        if (GetComponent<CityScript>() != null)
+        {
+            CityScript CS = GetComponent<CityScript>();
+            if (CS.buildings.Count > 0)
+            {
+                for (int i = 0; i < CS.buildings.Count; i++)
+                {
+                    CS.buildings[i].GetComponent<MeshCombiner>().RevertCombination();
+                }
+            }
+            GameObject StreetParent = GameObject.FindGameObjectWithTag("StreetParent");
+            MeshCombiner street_mesh_combiner = StreetParent.GetComponent<MeshCombiner>();
+            street_mesh_combiner.RevertCombination();
+        }
+    }
+}
---
+++ Assets/CityBuilderTool/Scripts/ResourcesManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourcesManager :MonoBehaviour
+{
+    public List<ResourcesData> resourcesData;
+}
+[System.Serializable]
+public struct ResourcesData
+{
+    public Texture2D Icon;
+    public GameObject cyan,blue,green,grey,orange,Red,purple,white;
+}
